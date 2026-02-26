36fa691a4dac1cae8eefff8ee35c3ba0c46c5ea6
sansavision <90557503+sansavision@users.noreply.github.com>
2026-02-26 01:19:53 +0100

    chore: bump version to 0.1.6-alpha.0 for release

--- build_dist.sh
+++ build_dist.sh
@@ -8,19 +8,19 @@ cd dist
 cp ../target/release/vidra ./vidra
 
 # macOS ARM
-tar -czvf vidra-v0.1.0-aarch64-apple-darwin.tar.gz vidra
+tar -czvf vidra-v0.1.6-alpha.0-aarch64-apple-darwin.tar.gz vidra
 
 # macOS x86 (dummy copy)
-cp vidra-v0.1.0-aarch64-apple-darwin.tar.gz vidra-v0.1.0-x86_64-apple-darwin.tar.gz
+cp vidra-v0.1.6-alpha.0-aarch64-apple-darwin.tar.gz vidra-v0.1.6-alpha.0-x86_64-apple-darwin.tar.gz
 
 # Linux x86 (dummy copy)
-cp vidra-v0.1.0-aarch64-apple-darwin.tar.gz vidra-v0.1.0-x86_64-unknown-linux-musl.tar.gz
+cp vidra-v0.1.6-alpha.0-aarch64-apple-darwin.tar.gz vidra-v0.1.6-alpha.0-x86_64-unknown-linux-musl.tar.gz
 
 # Linux ARM (dummy copy)
-cp vidra-v0.1.0-aarch64-apple-darwin.tar.gz vidra-v0.1.0-aarch64-unknown-linux-musl.tar.gz
+cp vidra-v0.1.6-alpha.0-aarch64-apple-darwin.tar.gz vidra-v0.1.6-alpha.0-aarch64-unknown-linux-musl.tar.gz
 
 # Windows x86 (dummy copy)
 cp vidra vidra.exe
-zip vidra-v0.1.0-x86_64-pc-windows-msvc.zip vidra.exe
+zip vidra-v0.1.6-alpha.0-x86_64-pc-windows-msvc.zip vidra.exe
 
 echo "Dist archives created."
